6bad84f7778916074d953eb16893aa17a48920a8
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2023-08-27 15:27:13 -0300

    Initial commit

---
+++ todo.js
@@ -0,0 +1,81 @@
+const todoList = [/* ,
+  {
+    name: "",
+    date: "",
+  } */
+];
+const btnEl = document.querySelector(".btn");
+const inputEl = document.getElementById("js-name-input");
+const dateEl = document.getElementById("js-date-input");
+const taskEl = document.querySelector(".js-task");
+
+
+function renderTodoList() {
+  let todoListHTML = "";
+  
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObj = todoList[i];
+    const name = todoObj.name;
+    const dueDate = todoObj.dueDate;
+    // Destructuring
+    /* const { name, dueDate } = todoObj; */
+    const html = `
+      <div>${name}</div> 
+      <div>${dueDate}</div> 
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      ">Delete</button>
+      `;
+    todoListHTML += html;
+  }
+  taskEl.innerHTML = todoListHTML;
+};
+
+function addTodo() {
+  const name = inputEl.value;
+  const dueDate = dateEl.value;
+  todoList.push({
+    name: name,
+    dueDate: dueDate
+    // Shorthand
+    /* name, 
+    dueDate */
+  });
+  renderTodoList();
+  inputEl.value = '';
+  dateEl.value = '';
+  inputEl.focus();
+};
+
+btnEl.addEventListener('click', () => {
+  addTodo()
+});
+
+
+// Loops
+
+/*  let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}; */
+
+/* let i = 1
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 6) */
+
+/* for (i = 1; i <= 7; i++) {
+  console.log(i);
+}; */
+
+/* let randomNum = 0;
+
+while (randomNum < 0.5) {
+  randomNum = Math.random();
+  console.log(randomNum);
+} */
